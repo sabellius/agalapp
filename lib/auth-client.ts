094637e3a8c90d93937b9f7785040cfe00ca11ec
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2026-01-07 15:23:11 +0200

    feat: export useSession

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -4,3 +4,5 @@ export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
 });
+
+export const { useSession } = authClient;
