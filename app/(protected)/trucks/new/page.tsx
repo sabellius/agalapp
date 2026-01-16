3fe26bd221f23a8f6c140b0edfec5e846c61237a
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2026-01-16 16:04:41 +0200

    feat: add an add new truck page

---
+++ app/(protected)/trucks/new/page.tsx
@@ -0,0 +1,9 @@
+import { TruckForm } from "@/components/trucks/truck-form";
+
+export default async function NewTruckPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <TruckForm />
+    </div>
+  );
+}
